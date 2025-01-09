f4b638368a3c3adabe8d814d3756fe8a3f328710
T.Mohana Sai <tunuguntlamohanasai@Mohanas-MacBook-Air.local>
2025-01-09 11:59:26 +0530

    Added styling
    how to add styling using ejs created a public folder and added static files for expess using app.use(express.static())

--- template-engine/index.js
+++ template-engine/index.js
@@ -3,7 +3,11 @@ const app = express();
 const path = require('path');
 app.set('view engine','ejs');
 // if view folder is not there and we are using another folder then we use this and also we require path
+// the above line will also do the same work but it should be in same folder
 app.set('views',path.join(__dirname,'views'));
+// to add css after creating a public folder which contains static files
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/',(req,res)=>{
  res.send("run")
 })
