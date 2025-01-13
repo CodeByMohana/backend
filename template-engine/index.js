8e3f1cb81e9faa9c5b95fc0c47bb2d2ea0702f79
T.Mohana Sai <137144689+CodeByMohana@users.noreply.github.com>
2025-01-13 10:49:33 +0530

    jan13

--- template-engine/index.js
+++ template-engine/index.js
@@ -8,6 +8,8 @@ app.set('views',path.join(__dirname,'views'));
 // to add css after creating a public folder which contains static files
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded({extended:true}));
+
 app.get('/',(req,res)=>{
  res.send("run")
 })
@@ -16,7 +18,16 @@ app.get('/about',(req,res)=>{
 })
 app.get('/login',(req,res)=>{
     res.render('login');
+    console.log(req.body);
 })
+app.get('/signup',(req,res)=>{
+    res.render('signup');
+})
+app.get('/home',(req,res)=>{
+    console.log(req.query);
+    res.send('ok');
+});
+app.get
 app.listen(4001,()=>{
     console.log('server is running on port 4001');
 })
