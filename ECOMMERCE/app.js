6e501167a4c36c73351930d540d34c88b76893c9
T.Mohana Sai <137144689+CodeByMohana@users.noreply.github.com>
2025-03-25 11:44:44 +0530

    updated

--- ECOMMERCE/app.js
+++ ECOMMERCE/app.js
@@ -5,6 +5,10 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const joi = require("joi");
 var methodOverride = require("method-override");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./model/user.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -14,20 +18,28 @@ app.use(methodOverride("_method"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/Ecommerce")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-const Product = require("./model/Product");
-const productSchema = require("./joiSchema");
-
-const productRoutes = require('./routes/product');
-app.use(productRoutes)
-
-
+// Routes
+const productRoutes = require("./routes/product");
+app.use(productRoutes);
 
 const Port = 3005;
 app.listen(Port, () => console.log(`Server is running on port ${Port}`));
